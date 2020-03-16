cf84552bee60ac416cafa191da5400d24321803e
thomas <thomas@localhost>
2020-03-16 19:32:06 +0100

    Test Workflow
    Warum nicht stream-API nutzen? Soll das mit Java < 1.8 laufen?

--- src/dataCrunch/PlayerPool.java
+++ src/dataCrunch/PlayerPool.java
@@ -5,28 +5,28 @@ import java.util.List;
 
 import dataTypes.Player;
 
-public class PlayerPool
-{
+public class PlayerPool {
 
 	private static final List<Player> players = new ArrayList<>();
-	
-	public static Player getOrCreatePlayer(String name)
-	{
-		for(Player player : players)
-		{
-			if(player.getName().equals(name))
-			{
-				return player;
-			}
-		}
+
+	public static Player getOrCreatePlayer(String name) {
+
+		players.stream()
+				.filter(p -> p.getName().equals(name))
+				.findFirst()
+				.orElse(createPlayer(name));
+
+		return createPlayer(name);
+	}
+
+	private static Player createPlayer(String name) {
 		Player player = new Player(name);
 		players.add(player);
 		return player;
 	}
-	
-	public static List<Player> getAllPlayers()
-	{
+
+	public static List<Player> getAllPlayers() {
 		return players;
 	}
-	
+
 }
