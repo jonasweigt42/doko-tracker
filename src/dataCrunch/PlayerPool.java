1aa9dd76c26d34494e0372c4df2bf58e71882149
[WEIGT Jonas] <[jonas.weigt@spar-ics.com]>
2020-03-17 11:19:51 +0100

    changed format

--- src/dataCrunch/PlayerPool.java
+++ src/dataCrunch/PlayerPool.java
@@ -5,26 +5,25 @@ import java.util.List;
 
 import dataTypes.Player;
 
-public class PlayerPool {
+public class PlayerPool
+{
 
 	private static final List<Player> players = new ArrayList<>();
 
-	public static Player getOrCreatePlayer(String name) {
-
-		return players.stream()
-				.filter(p -> p.getName().equals(name))
-				.findFirst()
-				.orElseGet(() -> createPlayer(name));
-
+	public static Player getOrCreatePlayer(String name)
+	{
+		return players.stream().filter(p -> p.getName().equals(name)).findFirst().orElseGet(() -> createPlayer(name));
 	}
 
-	private static Player createPlayer(String name) {
+	private static Player createPlayer(String name)
+	{
 		Player player = new Player(name);
 		players.add(player);
 		return player;
 	}
 
-	public static List<Player> getAllPlayers() {
+	public static List<Player> getAllPlayers()
+	{
 		return players;
 	}
 
